dab45205a7168b038a409ee9eed700033ca8796a
RauffA011 <121196198+RauffA011@users.noreply.github.com>
2024-05-30 15:25:14 -0400

    commit for packages page

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+         </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
